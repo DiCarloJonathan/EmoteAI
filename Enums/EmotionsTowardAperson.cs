dd9e434d93dac7957114dee7aca629cae4e4b73b
di_carlo_jonathan <dicarlojonathan@gmal.com>
2017-09-26 11:05:05 -0700

    Initial commit Fear and surprise enums

---
+++ Enums/EmotionsTowardAperson.cs
@@ -0,0 +1,24 @@
+[ComVisibleAttribute(true)]
+public static class EmotionEnums
+{
+public enum Fear
+{
+    none = 0,
+    apprehention = 1,
+    fear = 2,
+    terror = 3, 
+    overpowering = 4
+}
+public enum Surprise
+{
+    none = 0,
+    distraction = 1,
+    surprise = 2,
+    Amazement = 3, 
+    overpowering = 4
+}
+
+
+
+
+}
